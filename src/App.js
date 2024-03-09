f685007fe2c51e5fb58faa1e189efe724ba5cf69
syeda_hani <syedaummehani.m@gmail.com>
2024-03-09 11:11:19 +0530

    in file saving

--- src/App.js
+++ src/App.js
@@ -4,53 +4,53 @@ import { BrowserRouter, Routes, Route,useNavigationType,useLocation, } from "rea
 import QnA_page from './Components/QnA_page';
 import Home from './Components/Home';
 
-  import BetadayzerovercelappByHtm from "./Components/BetadayzerovercelappByHtm";
+  import BetadayzerovercelappByHtm from "./Components/BetadayzerovercelappByHtm.jsx";
   import { useEffect } from "react";
   import Login  from "./Components/Login"
 
 function App() {
-	const action = useNavigationType();
-  const location = useLocation();
-  const pathname = location.pathname;
+	// const action = useNavigationType();
+  // const location = useLocation();
+  // const pathname = location.pathname;
 
-  useEffect(() => {
-    if (action !== "POP") {
-      window.scrollTo(0, 0);
-    }
-  }, [action, pathname]);
+  // useEffect(() => {
+  //   if (action !== "POP") {
+  //     window.scrollTo(0, 0);
+  //   }
+  // }, [action, pathname]);
 
-  useEffect(() => {
-    let title = "";
-    let metaDescription = "";
+  // useEffect(() => {
+  //   let title = "";
+  //   let metaDescription = "";
 
-    switch (pathname) {
-      case "/":
-        title = "";
-        metaDescription = "";
-        break;
-    }
+  //   switch (pathname) {
+  //     case "/":
+  //       title = "";
+  //       metaDescription = "";
+  //       break;
+  //   }
 
-    if (title) {
-      document.title = title;
-    }
+  //   if (title) {
+  //     document.title = title;
+  //   }
 
-    if (metaDescription) {
-      const metaDescriptionTag = document.querySelector(
-        'head > meta[name="description"]'
-      );
-      if (metaDescriptionTag) {
-        metaDescriptionTag.content = metaDescription;
-      }
-    }
-  }, [pathname]);
+  //   if (metaDescription) {
+  //     const metaDescriptionTag = document.querySelector(
+  //       'head > meta[name="description"]'
+  //     );
+  //     if (metaDescriptionTag) {
+  //       metaDescriptionTag.content = metaDescription;
+  //     }
+  //   }
+  // }, [pathname]);
 	return (
 		<div>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/QnA_page" element={<QnA_page />} />
-					<Route path="/signup" element={<BetadayzerovercelappByHtm />} />
-      <Route path="/login" element={<Login/>}/>
+					{/* <Route path="/" element={<BetadayzerovercelappByHtm />} />
+      <Route path="/login" element={<Login/>}/> */}
 				</Routes>
 			</BrowserRouter>
 		</div>
